14da02254906103fdef3a8b784deba4da827f2e6
BR-Oleg <guilherme.rabello079@gmail.com>
2025-11-13 20:53:53 -0300

    m

--- ios_native/Sources/SafePlateNative/MapboxManager.swift
+++ ios_native/Sources/SafePlateNative/MapboxManager.swift
@@ -1,15 +1,6 @@
 import Foundation
-#if canImport(MapboxMaps)
-import MapboxMaps
 
 enum MapboxManager {
-    static func configure() {
-        let token = "pk.eyJ1Ijoic2FmZXBsYXRlNTAwIiwiYSI6ImNtaGZoMXF2NTA1dDIya3B5dnljbXkzZG4ifQ.DgeBcy0YXvBdDLdPVerqjA"
-        ResourceOptionsManager.default.resourceOptions.accessToken = token
-    }
+    static let accessToken = "pk.eyJ1Ijoic2FmZXBsYXRlNTAwIiwiYSI6ImNtaGZoMXF2NTA1dDIya3B5dnljbXkzZG4ifQ.DgeBcy0YXvBdDLdPVerqjA"
+    static func configure() { /* no-op; token passed via MapInitOptions */ }
 }
-#else
-enum MapboxManager {
-    static func configure() { /* no-op */ }
-}
-#endif
--- ios_native/Sources/SafePlateNative/Views/MapboxMapView.swift
+++ ios_native/Sources/SafePlateNative/Views/MapboxMapView.swift
@@ -5,7 +5,8 @@ import CoreLocation
 struct MapboxMapView: UIViewRepresentable {
     func makeUIView(context: Context) -> MapView {
         MapboxManager.configure()
-        let options = MapInitOptions(cameraOptions: CameraOptions(center: CLLocationCoordinate2D(latitude: -23.5505, longitude: -46.6333), zoom: 11))
+        let resourceOptions = ResourceOptions(accessToken: MapboxManager.accessToken)
+        let options = MapInitOptions(resourceOptions: resourceOptions, cameraOptions: CameraOptions(center: CLLocationCoordinate2D(latitude: -23.5505, longitude: -46.6333), zoom: 11))
         let mapView = MapView(frame: .zero, mapInitOptions: options)
         return mapView
     }
