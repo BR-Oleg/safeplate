7737e3c2700ae786d7d48a791801dc42dc52c93c
BR-Oleg <guilherme.rabello079@gmail.com>
2025-11-14 16:48:41 -0300

    m

--- ios_native/Sources/SafePlateNative/Views/MapboxMapView.swift
+++ ios_native/Sources/SafePlateNative/Views/MapboxMapView.swift
@@ -4,16 +4,15 @@ import CoreLocation
 #if canImport(MapboxMaps) && USE_MAPBOX
 import MapboxMaps
 
-struct MapboxMapView: UIViewRepresentable {
-    func makeUIView(context: Context) -> MapView {
-        MapboxManager.configure()
-        let resourceOptions = ResourceOptions(accessToken: MapboxManager.accessToken)
-        let options = MapInitOptions(resourceOptions: resourceOptions, cameraOptions: CameraOptions(center: CLLocationCoordinate2D(latitude: -23.5505, longitude: -46.6333), zoom: 11))
-        let mapView = MapView(frame: .zero, mapInitOptions: options)
-        return mapView
+struct MapboxMapView: View {
+    var body: some View {
+        Map(
+            initialViewport: .camera(
+                center: CLLocationCoordinate2D(latitude: -23.5505, longitude: -46.6333),
+                zoom: 11
+            )
+        )
     }
-
-    func updateUIView(_ uiView: MapView, context: Context) {}
 }
 #else
 struct MapboxMapView: View {
