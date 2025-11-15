a9da8259ab52e6b649d7e9c4456e5803002d737f
BR-Oleg <guilherme.rabello079@gmail.com>
2025-11-15 15:59:44 -0300

    m

--- ios_native/Sources/SafePlateNative/FirebaseManager.swift
+++ ios_native/Sources/SafePlateNative/FirebaseManager.swift
@@ -7,9 +7,25 @@ enum FirebaseManager {
         if FirebaseApp.app() != nil { return }
         if Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") != nil {
             FirebaseApp.configure()
-        } else {
-            print("FirebaseManager: GoogleService-Info.plist not found. Running without Firebase.")
+            if FirebaseApp.app() != nil { return }
         }
+
+        #if DEBUG
+        if FirebaseApp.app() == nil {
+            let options = FirebaseOptions(
+                googleAppID: "1:476899420653:ios:3c8037d0a8fae5e103dfe4",
+                gcmSenderID: "476899420653"
+            )
+            options.apiKey = "AIzaSyD6BrhUmXeqFYiPnNbVbxkxvxwnyqNrzQ8"
+            options.projectID = "safeplate-a14e9"
+            options.storageBucket = "safeplate-a14e9.firebasestorage.app"
+            FirebaseApp.configure(options: options)
+        }
+        #else
+        if FirebaseApp.app() == nil {
+            print("FirebaseManager: GoogleService-Info.plist not found or configure() failed. Running without Firebase.")
+        }
+        #endif
     }
 }
 #else
